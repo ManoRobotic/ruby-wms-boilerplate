55563bb47c8eac631f40a6cfa1ca60f9faf1395b
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2024-10-02 17:54:30 -0600

    feat: add images to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_24_192951) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_26_193128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -32,5 +32,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_24_192951) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Category.destroy_all
+r
+Category.create(name: "Oro", description: "Monedas de Oro", image: "https://www.banxico.org.mx/multimedia/libertadOroFrente.png")
+Category.create(name: "Plata", description: "Monedas de Plata", image: "https://www.banxico.org.mx/multimedia/NvaSerLibPlaRev.png")
+Category.create(name: "Numismática", description: "Monedas Numismática", image: "https://www.banxico.org.mx/ColeccionNumismatica/resources/images/bienvenida/imgs_menu_monedas.png")
+
+puts "Finished seeding Categories"
