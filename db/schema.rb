c11547c4176d056efacdf5f52fbd2db35851833e
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2024-11-07 02:32:44 -0600

    feat: add order - products model relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_05_070550) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_07_043048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -64,6 +64,17 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_05_070550) do
     t.string "image_url"
   end
 
+  create_table "order_products", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "product_id", null: false
+    t.uuid "order_id", null: false
+    t.string "size", null: false
+    t.integer "quantity", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
   create_table "orders", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "customer_email", null: false
     t.boolean "fulfilled"
@@ -95,6 +106,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_05_070550) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
   add_foreign_key "products", "categories"
   add_foreign_key "stocks", "products"
 end
