5fb6ba6babff023d02e901d22bccfa484245eca4
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2024-11-06 19:36:15 -0600

    feat: show orders in a table

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -58,12 +58,10 @@ class Admin::OrdersController < AdminController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_order
       @admin_order = Order.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def order_params
       params.require(:order).permit(:customer_email, :fulfilled, :total, :address)
     end
