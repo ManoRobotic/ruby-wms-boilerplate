e66082f981ef875204ee5fdc85286ade5460df6a
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2024-11-20 02:50:19 -0600

    feat: add chart js importmap

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -1,12 +1,47 @@
 import { Controller } from "@hotwired/stimulus"
-import { Chart, registrables } from 'chart.js'
-
-Chart.register(...registrables)
-
+import {Chart, registerables } from 'chart.js'
+Chart.register(...registerables)
 // Connects to data-controller="dashboard"
 export default class extends Controller {
+  static values = { revenue: Array }
   initialize() {
-    const data = [10 ,20 ,30 ,40 ,50 ,60 ,70, 80]
-    const labels = ['Lun', 'Mar', 'Mier', 'Jue', 'Vie', 'Sab', 'Dom']
+    const data = this.revenueValue.map((item) => item[1]/100.0)
+    const labels = this.revenueValue.map((item) => item[0])
+    const ctx = document.getElementById('revenueChart')
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Revenue $',
+          data: data,
+          borderWidth: 3,
+          fill: true
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false
+          }
+        },
+        scales: {
+          x: {
+            grid: {
+              display: false
+            }
+          },
+          y: {
+            border: {
+              dash: [5, 5]
+            },
+            grid: {
+              color: "#d4f3ef"
+            },
+            beginAtZero: true
+          }
+        }
+      }
+    })
   }
 }
