ed675d7374efb9e0a27558efdaaeec7a781b8a86
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2025-06-02 10:25:49 -0600

    modified stats admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,18 +10,14 @@ class AdminController < ApplicationController
       avg_sale: Order.where(created_at: Time.now.midnight..Time.now)&.average(:total),
       per_sale: OrderProduct.joins(:order).where(orders: {created_at: Time.now.midnight..Time.now})&.average(:quantity),
     }
-    @orders_by_day = Order.where('created_at > ?', Time.now - 7.days).order(:created_at)
-    @orders_by_day = @orders_by_day.group_by { |order| order.created_at.to_date }
-    @revenue_by_day = @orders_by_day.map { |day, orders| [day.strftime("%A"), orders.sum(&:total)] }
-    if @revenue_by_day.count < 7
-      days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-      data_hash = @revenue_by_day.to_h
-      current_day = Date.today.strftime("%A")
-      current_day_index = days_of_week.index(current_day)
-      next_day_index = (current_day_index + 1) % days_of_week.count
-      ordered_days_with_current_last = days_of_week[current_day_index..-1] + days_of_week[0...next_day_index]
-      complete_ordered_array_with_current_last = ordered_days_with_current_last.map{ |day| [day, data_hash.fetch(day, 0)] }
-      @revenue_by_day = complete_ordered_array_with_current_last
+    last_7_days = (6.days.ago.to_date..Date.today).to_a
+    @orders_by_day = Order.where('created_at >= ?', 7.days.ago.beginning_of_day)
+                          .group_by { |order| order.created_at.to_date }
+    @revenue_by_day = last_7_days.map do |date|
+      day_name = date.strftime("%A")
+      orders_for_day = @orders_by_day[date] || []
+      revenue = orders_for_day.sum(&:total)
+      [day_name, revenue]
     end
   end
 end
