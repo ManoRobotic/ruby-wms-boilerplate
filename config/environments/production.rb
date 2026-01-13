361337c717012c3b57affaa4f0b8efe838a43c0c
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2026-01-12 18:33:41 -0600

    fix: fix socket connection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,9 +40,9 @@ Rails.application.configure do
   config.active_storage.service = :aws
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.mount_path = "/cable"
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = [ nil, "https://wmsys.fly.dev", /https:\/\/wmsys\.fly\.dev.*/ ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
