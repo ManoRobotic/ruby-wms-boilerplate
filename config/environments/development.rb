89c20c03d22a69bd4c2afebb4f7cc3a9c84ba52e
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2026-03-01 22:26:09 -0600

    add development url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,10 +77,11 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   config.action_cable.disable_request_forgery_protection = true
 
-  # Allow all hosts (including ngrok)
+  # Allow all hosts (including ngrok and docker)
   config.hosts.clear
   config.hosts << /.*\.ngrok-free\.app/
   config.hosts << "localhost"
+  config.hosts << "0.0.0.0"
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
